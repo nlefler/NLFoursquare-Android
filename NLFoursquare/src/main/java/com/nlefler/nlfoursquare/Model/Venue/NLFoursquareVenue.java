1a9bbb2a10483db35d4fb2cfef3e4c054f1718d1
Nathan Lefler <n.lefler@gmail.com>
2014-05-25 00:35:54 -0400

    Add more fields to venue model

--- NLFoursquare/src/main/java/com/nlefler/nlfoursquare/Model/Venue/NLFoursquareVenue.java
+++ NLFoursquare/src/main/java/com/nlefler/nlfoursquare/Model/Venue/NLFoursquareVenue.java
@@ -1,8 +1,18 @@
 package com.nlefler.nlfoursquare.Model.Venue;
 
+import java.util.List;
+
 /**
  * Created by Nathan Lefler on 5/19/14.
  */
 public class NLFoursquareVenue {
+    public String id;
     public String name;
+    public NLFoursquareVenueContact contact;
+    public NLFoursquareLocation location;
+    public List<NLFoursquareCategory> categories;
+    public boolean verified;
+    public NLFoursquareVenueStats stats;
+
+    public String url;
 }
