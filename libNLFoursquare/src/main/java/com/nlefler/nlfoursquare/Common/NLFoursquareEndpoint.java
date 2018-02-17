233431736b2adc9135a255a9d7766aeaf48dcd3f
Nathan Lefler <nlefler@users.noreply.github.com>
2018-02-17 13:25:48 -0500

    Update NLFoursquareEndpoint.java

--- libNLFoursquare/src/main/java/com/nlefler/nlfoursquare/Common/NLFoursquareEndpoint.java
+++ libNLFoursquare/src/main/java/com/nlefler/nlfoursquare/Common/NLFoursquareEndpoint.java
@@ -4,5 +4,5 @@ package com.nlefler.nlfoursquare.Common;
  * Created by Nathan Lefler on 12/29/14.
  */
 public class NLFoursquareEndpoint {
-    public static final String NLFOURSQUARE_V2_ENDPOINT = "https://api.foursquare.com/v2";
+    public static final String NLFOURSQUARE_V2_ENDPOINT = "https://api.foursquare.com/v2/";
 }
